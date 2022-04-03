02f8fa0eb5fd8dca73910a8e404b8581b59f97b0
Etu14 <perdochjakub@gmail.com>
2022-04-03 18:54:25 +0200

    Pohyb hráča

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ SCREEN_HEIGHT= 600
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption('Gotham')
 
+#framerate
+clock=pygame.time.Clock()
+FPS= 60
+
 #load images
 bg_image = pygame.image.load("assets/bg.jpg").convert_alpha()
 batman_image = pygame.image.load("assets/batman.png").convert_alpha()
@@ -29,9 +33,37 @@ class Player():
         self.height=75 
         self.rect= pygame.Rect(0,0, self.width, self.height)
         self.rect.center =(x,y)
+        self.flip=False
     
+    def move(self):
+
+        #reset variables
+        dx=0
+        dy=0
+
+        #process keypresses
+        key = pygame.key.get_pressed()
+        if key[pygame.K_a]:
+            dx= -10
+            self.flip=True
+        if key[pygame.K_d]:
+            dx= +10
+            self.flip=False
+
+        #ensure player doesnt go off the edge of the screen
+        if self.rect.left +dx <0:
+            dx=- self.rect.left
+        
+        if self.rect.right +dx > SCREEN_WIDTH:
+            dx= SCREEN_WIDTH - self.rect.right
+
+        #update rect position
+        self.rect.x +=dx
+        self.rect.y +=dy
+       
+
     def draw(self):
-        screen.blit(self.image,(self.rect.x-20,self.rect.y-5))
+        screen.blit(pygame.transform.flip(self.image,self.flip, False),(self.rect.x-20,self.rect.y-5))
         pygame.draw.rect(screen,WHITE,self.rect,2)
 
 batman = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150 )
@@ -39,6 +71,10 @@ batman = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150 )
 #game loop
 run = True
 while run:
+
+    #movement
+    batman.move()
+    clock.tick(FPS)
 
     #draw bg
     screen.blit(bg_image, (0, 0))
