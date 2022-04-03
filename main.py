157205c857b9e81639eaf157151fd135ca0250e2
Etu14 <perdochjakub@gmail.com>
2022-04-03 17:49:21 +0200

    pridal som postavu

--- main.py
+++ main.py
@@ -14,7 +14,27 @@ pygame.display.set_caption('Gotham')
 
 #load images
 bg_image = pygame.image.load("assets/bg.jpg").convert_alpha()
+batman_image = pygame.image.load("assets/batman.png").convert_alpha()
 
+
+#define colors
+WHITE=(255,255,255)
+
+
+#player class
+class Player():
+    def __init__(self,x,y):
+        self.image=pygame.transform.scale(batman_image,(90,90))
+        self.width= 45
+        self.height=75 
+        self.rect= pygame.Rect(0,0, self.width, self.height)
+        self.rect.center =(x,y)
+    
+    def draw(self):
+        screen.blit(self.image,(self.rect.x-20,self.rect.y-5))
+        pygame.draw.rect(screen,WHITE,self.rect,2)
+
+batman = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150 )
 
 #game loop
 run = True
@@ -22,6 +42,9 @@ while run:
 
     #draw bg
     screen.blit(bg_image, (0, 0))
+
+    #draw sprites
+    batman.draw()
     
     #event handler
     for event in pygame.event.get():
